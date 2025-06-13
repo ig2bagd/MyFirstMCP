3236cc82513cc38fc2e5be0ad4060da688bc13c4
steviep <ig2bagd@yahoo.com>
2025-06-12 20:23:07 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MyFirstMCP;
+
+
+// https://devblogs.microsoft.com/dotnet/build-a-model-context-protocol-mcp-server-in-csharp/
+// https://www.youtube.com/watch?v=Coot4TFTkN4
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+  // Configure all logs to go to stderr
+  consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<MonkeyService>();
+
+await builder.Build().RunAsync();
